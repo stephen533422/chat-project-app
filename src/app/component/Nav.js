eaa4d585ef94282db23caea1f9a77ddf732869a9
Chao-Ting Ho <stephen533422@gmail.com>
2023-12-08 02:55:59 +0800

    [fix] Nav RWD & handle details

--- src/app/component/Nav.js
+++ src/app/component/Nav.js
@@ -5,17 +5,19 @@ import classNames from "classnames";
 import styles from '@/app/component/nav.module.scss';
 import Link  from 'next/link';
 import { AuthContext } from "@/context/AuthContext";
-import { useRouter } from "next/navigation";
+import { useRouter,usePathname } from "next/navigation";
 
 function Nav() {
     const {user}=useContext(AuthContext);
     const router = useRouter();
-    
+    const path = usePathname();
+    // console.log(path);
     
     return (
         <>
             <div className={styles.nav}>
-                <div className={classNames(styles.indexicon)}>
+                <div className={classNames(styles.indexicon)} onClick={()=>router.push("/")}>
+                    <img src={"/chat.png"}></img>
                     <h2>InstantChat</h2>
                 </div>
                 {user && <div className={styles.navUser} onClick={()=>router.push("profile")}>
@@ -24,25 +26,46 @@ function Nav() {
                 </div>}
                 <Link href="/friends">
                     <div className={styles.navItem}>
-                        好友
+                        {path==="/friends" 
+                            ?<img src="/nav/friends-dark.png"></img>
+                            :<img src="/nav/friends.png"></img>
+                        }
+                        <div className={styles.title}>好友</div>
                     </div>
                 </Link>
                 <Link href="/chats">
                     <div className={styles.navItem}>
-                        聊天
+                        {path==="/chats" 
+                            ?<img src="/nav/chats-dark.png"></img>
+                            :<img src="/nav/chats.png"></img>
+                        }
+                        <div className={styles.title}>聊天</div>
                     </div>
                 </Link>
                 <Link href="/posts">
                     <div className={styles.navItem}>
-                        貼文
+                        {path==="/posts" 
+                                ?<img src="/nav/posts-dark.png"></img>
+                                :<img src="/nav/posts.png"></img>
+                        }
+                        <div className={styles.title}>貼文</div>
                     </div>
                 </Link>
                 <Link href="/settings">
                     <div className={styles.navItem}>
-                        設定
+                        {path==="/settings" 
+                                ?<img src="/nav/settings-dark.png"></img>
+                                :<img src="/nav/settings.png"></img>
+                        }
+                        <div className={styles.title}>設定</div>
                     </div>
                 </Link>
-                <button className={classNames(styles.btn)} onClick={()=>{Logout();router.push("/");}}>登出</button>
+                <div className={styles.navContainer}>
+                    <button className={classNames(styles.btn)} onClick={()=>{Logout();router.push("/");}}>
+                        登出
+                        <img></img>
+                    </button>
+                </div>
             </div>
         </>
     );
